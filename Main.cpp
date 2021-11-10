74bfa5177903234a7c1d27425e2ce4d7f1cbacb4
L1oid <71085120+L1oid@users.noreply.github.com>
2021-11-10 14:23:03 +0700

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,218 @@
+﻿#include <iostream>
+#include <string>
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+using namespace std;
+
+class ShapeLC
+{
+protected:
+	string str;
+	float x, y;
+public:
+	ShapeLC()
+	{
+		x = 0;
+		y = 0;
+	}
+	virtual ~ShapeLC() {};
+	virtual string to_String() = 0;
+	virtual float Area() = 0;
+	virtual void Draw() = 0;
+	void Move(float move_x, float move_y)
+	{
+		x = move_x;
+		y = move_y;
+	}
+};
+
+class Triangle : public ShapeLC
+{
+private:
+	float a, b, c, p, r, r_pixel;
+public:
+	Triangle() 
+	{
+		a = 0;
+		b = 0;
+		c = 0;
+		p = 0;
+		r = 0;
+		r_pixel = 0;
+	}
+	Triangle(float a_p, float b_p, float c_p)
+	{
+		a = a_p;
+		b = b_p;
+		c = c_p;
+		p = (a + b + c) / 2;
+		r = (a * b * c) / (4 * sqrt(p * (p - a) * (p - b) * (p - c)));
+		r_pixel = r * 37.795275590551;
+	}
+	virtual ~Triangle() {};
+	virtual float Area()
+	{
+		return sqrt(p * (p - a) * (p - b) * (p - c));
+	}
+	virtual string to_String()
+	{
+		str += "Triangle info: ";
+		str += "Side a: ";
+		str += to_string(a);
+		str += " Side b: ";
+		str += to_string(b);
+		str += " Side c: ";
+		str += to_string(c);
+		str += " Semi-perimeter: ";
+		str += to_string(p);
+		str += " Area: ";
+		str += to_string(Area());
+		str += "\n";
+		return str;
+	}
+	virtual void Draw()
+	{
+		RenderWindow window(VideoMode(500, 200), "SFML Works!");
+		while (window.isOpen())
+		{
+			Event event;
+			while (window.pollEvent(event))
+			{
+				if (event.type == Event::Closed)
+					window.close();
+			}
+			window.clear(Color(250, 220, 100, 0));
+			CircleShape triangle(r_pixel, 3);
+			triangle.setPosition(x, y);
+			triangle.setFillColor(Color::Blue);
+			window.draw(triangle);
+			window.display();
+		}
+	}
+};
+
+class Rectangle : public ShapeLC
+{
+protected:
+	float a, b, a_pixel, b_pixel;
+public:
+	Rectangle()
+	{
+		a = 0;
+		b = 0;
+		a_pixel = 0;
+		b_pixel = 0;
+	}
+	Rectangle(float a_p, float b_p)
+	{
+		a = a_p;
+		b = b_p;
+		a_pixel = a * 37.795275590551;
+		b_pixel = b * 37.795275590551;
+	}
+	virtual ~Rectangle() {};
+	virtual float Area()
+	{
+		return a * b;
+	}
+	virtual string to_String()
+	{
+		str += "Rectangle info: ";
+		str += "Side a: ";
+		str += to_string(a);
+		str += " Side b: ";
+		str += to_string(b);
+		str += " Area: ";
+		str += to_string(Area());
+		str += "\n";
+		return str;
+	}
+	virtual void Draw()
+	{
+		RenderWindow window(VideoMode(500, 200), "SFML Works!");
+		while (window.isOpen())
+		{
+			Event event;
+			while (window.pollEvent(event))
+			{
+				if (event.type == Event::Closed)
+					window.close();
+			}
+			window.clear(Color(250, 220, 100, 0));
+			RectangleShape rectangle(Vector2f(a_pixel , b_pixel));
+			rectangle.setPosition(x, y);
+			rectangle.setFillColor(Color(175, 180, 240));
+			window.draw(rectangle);
+			window.display();
+		}
+	}
+};
+
+class Square : public Rectangle
+{
+public:
+	Square(float a_p) : Rectangle(a_p, a_p) {};
+	virtual ~Square() {};
+	virtual string to_String()
+	{
+		str += "Square info: ";
+		str += "Side a: ";
+		str += to_string(a);
+		str += " Area: ";
+		str += to_string(Area());
+		str += "\n";
+		return str;
+	}
+};
+
+int main()
+{
+	ShapeLC* shp;
+	int key = 0;
+	do
+	{
+		cout << "1) Triangle" << endl
+			<< "2) Rectangle" << endl
+			<< "3) Square" << endl
+			<< "4) " << endl
+			<< "5) " << endl
+			<< "0) Exit" << endl;
+		cout << endl << "Select an action: ";
+		cin >> key;
+		cout << endl;
+		switch (key)
+		{
+		case 1:
+			shp = new Triangle(3, 3, 3);
+			cout << shp->to_String() << endl;
+			shp->Move(2, 2);
+			shp->Draw();
+			break;
+		case 2:
+			shp = new Rectangle(3, 5);
+			cout << shp->to_String() << endl;
+			shp->Move(4, 4);
+			shp->Draw();
+			break;
+		case 3:
+			shp = new Square(4);
+			cout << shp->to_String() << endl;
+			shp->Move(2, 2);
+			shp->Draw();
+			break;
+		case 4:
+
+			break;
+		case 5:
+
+			break;
+		default:
+			if (key != 0)
+			{
+				cout << "There is no such action !" << endl << endl;
+			}
+			break;
+		}
+	} while (key != 0);
+}
